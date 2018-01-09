2e01630002fd8b80e391b664a2c1e0fdb0da1d57
William Whitacre <w.whitacre@windgo.com>
2018-01-09 16:10:29 -0600

    Added two placeholder functions which forward to arraymancer_vision. These will be replaced as arraymancer_vision is absorbed so that only one dependency need be kept.

--- lerosi.nim
+++ lerosi.nim
@@ -1,11 +1,20 @@
 import system, sequtils, strutils, math, algorithm
+import haas, arraymancer, arraymancer_vision
+
+export haas, arraymancer
 
 # Fill this out with your own bootstrapping hooks. staticExec may be used
 # to conditionally run blocking scripts at compile time to prepare the source
 # tree before all sources are read by the compiler.
-when defined(haas_bootstrap):
-  static:
-    # Ensure that all included files from detail/ag exist in the tree.
-    # This executes a script which touches all such include files.
-    #echo(staticExec "cd haas/ag && sh ensure.sh")
+#when defined(haas_bootstrap):
+#  static:
+#    # Ensure that all included files from detail/ag exist in the tree.
+#    # This executes a script which touches all such include files.
+#    #echo(staticExec "cd haas/ag && sh ensure.sh")
+
+proc load_image*(filename: string, channels: int = 0): Tensor[uint8] =
+  load(filename, channels) # Simple forwarding at this point.
+
+proc load_image*(data: openarray[uint8], channels: int = 0): Tensor[uint8] =
+  loadFromMemory(filename, channels) # Simple forwarding at this point.
 
