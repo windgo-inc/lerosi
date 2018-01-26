4940c4d7ca737b271a4f3d03827bec385fc69379
William Whitacre <w.whitacre@windgo.com>
2018-01-26 13:55:34 -0600

    Committing compile-time color channel handling utilities.

---
+++ lerosi/channels.nim
@@ -0,0 +1,196 @@
+import os, system, macros, future
+import strutils, sequtils, sets, algorithm
+import fixedseq
+
+
+const MAX_CHANNELS = 16
+
+
+iterator capitalTokenIter(str: string): string =
+  var acc = newStringOfCap(str.len)
+
+  for i, ch in pairs(str):
+    if isUpperAscii(ch) and not acc.isNilOrEmpty:
+      yield acc
+      acc.setLen(0)
+    acc.add(ch)
+
+  # make sure to get the last one too
+  if not acc.isNilOrEmpty:
+    yield acc
+
+
+# Old interface
+proc capitalTokens(str: string): seq[string] {.inline.} =
+  result = newSeqOfCap[string](str.len)
+  for tok in capitalTokenIter(str):
+    result.add(tok)
+
+
+proc nodeToStr(node: NimNode): string {.compileTime.} =
+  case node.kind:
+    of nnkIdent:
+      result = $node
+    of nnkStrLit, nnkRStrLit:
+      result = node.strVal
+    else:
+      quit "Expected identifier or string as channel layout specifier, but got " & $node & "."
+  
+
+var channelLayoutIdCounter {.compileTime.} = 0
+var channelIdCounter {.compileTime.} = 0
+
+type
+  ChannelLayout* = object of RootObj
+  ChannelLayoutId* = distinct int
+  ChannelId = distinct int
+
+  ChannelNameArray* = FixedSeq[string, MAX_CHANNELS]
+
+var
+  channelLayoutNameSeq = newSeq[string]()
+  channelLayoutChannelsSeq = newSeq[ChannelNameArray]()
+
+
+proc emptyChannelNames*(): ChannelNameArray =
+  result.len = 0
+
+proc `$`(layout_id: ChannelLayoutId): string {.inline.} =
+  result = ["ChannelLayout(", $(layout_id.int), ")"].join
+
+proc name*(layout_id: ChannelLayoutId): string {.inline.} =
+  result = channelLayoutNameSeq[layout_id.int]
+
+proc channels*(layout_id: ChannelLayoutId): ChannelNameArray {.inline.} =
+  result = channelLayoutChannelsSeq[layout_id.int]
+
+proc len*(layout_id: ChannelLayoutId): int {.inline.} =
+  result = channelLayoutChannelsSeq[layout_id.int].len
+
+proc channel*(layout_id: ChannelLayoutId, name: string): int {.inline.} =
+  result = channelLayoutChannelsSeq[layout_id.int].find(name)
+
+
+proc declareChannelLayoutImpl(nameNode: NimNode): NimNode {.compileTime.} =
+  var id = channelLayoutIdCounter
+  inc(channelLayoutIdCounter)
+
+  let
+    name = nodeToStr(nameNode)
+
+    layoutIdent = ident(name)
+
+    chans = capitalTokens(`name`)
+    nCh = chans.len
+    maxCh = chans.len - 1
+
+  # Declare compile time inline procedures for fetching channel layout
+  # properties.
+  result = quote do:
+    type
+      `layoutIdent`* = object of ChannelLayout
+
+    proc id*(layout: typedesc[`layoutIdent`]): ChannelLayoutId {.inline, raises: [].} = ChannelLayoutId(`id`)
+    proc len*(layout: typedesc[`layoutIdent`]): Natural {.inline, raises: [].} = `nCh`
+    proc name*(layout: typedesc[`layoutIdent`]): string {.inline, raises: [].} = `name`
+    proc channels*(layout: typedesc[`layoutIdent`]): array[0..`maxCh`, string] {.inline, raises: [].} = `chans`
+    proc channel*(layout: typedesc[`layoutIdent`], name: string): int {.inline, raises: [].} = find(`chans`, name)
+
+  # Add the runtime channel layout table insertion code.
+  result.add(newCall(bindSym"add", [ident"channelLayoutNameSeq", newStrLitNode(name)]))
+  result.add(newCall(bindSym"add", [ident"channelLayoutChannelsSeq", newCall(bindSym"emptyChannelNames", [])]))
+
+  # Add named channel accessors procedures
+  var intermediate = newStmtList()
+  for i, name in chans:
+    let nameIdent = ident("Ch" & name)
+    let chIdIdent = ident("ChId" & name)
+    let nameLit = newStrLitNode(name)
+
+    let accessor = quote do:
+      when not declared(`nameIdent`):
+        # We must only declare this one time.
+        proc `nameIdent`*(layout_id: ChannelLayoutId): Natural =
+          layout_id.channel(`nameLit`)
+
+      proc `nameIdent`*(layout: typedesc[`layoutIdent`]): range[0..`maxCh`] = `i`
+
+    result.add(newCall(bindSym"add", [parseExpr"channelLayoutChannelsSeq[^1]", newStrLitNode(name)]))
+    accessor.copyChildrenTo(intermediate)
+
+  intermediate.copyChildrenTo(result)
+
+
+proc declareChannelLayoutPermuteImpl(nameNode: NimNode): NimNode {.compileTime.} =
+  var
+    channels = capitalTokens(nodeToStr(nameNode))
+
+  result = newStmtList()
+
+  sort(channels, system.cmp)
+  while true:
+    declareChannelLayoutImpl(newStrLitNode(channels.join)).copyChildrenTo(result)
+    
+    if not nextPermutation(channels):
+      break
+
+
+proc declareChannelGroupSeq(node: NimNode): NimNode {.compileTime.} =
+  result = newStmtList()
+  case node.kind:
+    of nnkStrLit, nnkRStrLit, nnkIdent:
+      declareChannelLayoutPermuteImpl(node).copyChildrenTo(result)
+    of nnkBracket:
+      for child in node.children:
+        declareChannelGroupSeq(child).copyChildrenTo(result)
+    else:
+      quit "Expected string literal, identifier, or bracketed expression."
+
+
+macro declareChannelGroups*(nameNodes: varargs[untyped]): untyped =
+  result = newStmtList()
+  for node in nameNodes:
+    declareChannelGroupSeq(node).copyChildrenTo(result)
+
+#proc getSwizzle(a: typedesc[])
+
+when isMainModule:
+  #expandMacros:
+  declareChannelGroups("RGB", "YUV", "YCbCr")
+  declareChannelGroups("RGBA", "YUVA", "YCbCrA")
+
+  template doRGBAProcs(what: untyped): untyped =
+    echo what.name, ".ChR = ", what.ChR, " ", what.name, ".channel(\"R\") = ", what.channel("R")
+    echo what.name, ".ChG = ", what.ChG, " ", what.name, ".channel(\"G\") = ", what.channel("G")
+    echo what.name, ".ChB = ", what.ChB, " ", what.name, ".channel(\"B\") = ", what.channel("B")
+    echo what.name, ".ChA = ", what.ChA, " ", what.name, ".channel(\"A\") = ", what.channel("A")
+
+  template doYCbCrProcs(what: untyped): untyped =
+    echo what.name, ".ChY  = ", what.ChY,  " ", what.name, ".channel(\"Y\")  = ", what.channel("Y")
+    echo what.name, ".ChCb = ", what.ChCb, " ", what.name, ".channel(\"Cb\") = ", what.channel("Cb")
+    echo what.name, ".ChCr = ", what.ChCr, " ", what.name, ".channel(\"Cr\") = ", what.channel("Cr")
+
+  static:
+    echo "*** COMPILE TIME TESTS ***"
+    template doTest(layoutType: typedesc, body: untyped): untyped =
+      echo "Testing ", layoutType.name, " (static type):"
+      echo layoutType.name, ".id = ", layoutType.id
+      echo layoutType.name, ".len = ", layoutType.len
+      echo layoutType.name, ".channels = ", @(layoutType.channels)
+      body
+    
+    doTest(RGBA): doRGBAProcs(RGBA)
+    doTest(BGRA): doRGBAProcs(BGRA)
+
+    doTest(YCbCr): doYCbCrProcs(YCbCr)
+    doTest(YCrCb): doYCbCrProcs(YCrCb)
+
+  echo "*** RUN TIME TESTS ***"
+  let myLayouts = [RGBA.id, BGRA.id, YCbCr.id, YCrCb.id]
+  for i, layout in myLayouts:
+    echo "Testing ", layout.name, " ", layout, ":"
+    echo layout.name, ".len = ", layout.len
+    echo layout.name, ".channels = ", @(layout.channels)
+    if i > 1: doYCbCrProcs(layout) else: doRGBAProcs(layout)
+    
+
---
+++ lerosi/fixedseq.nim
@@ -0,0 +1,187 @@
+# Copyright 2017 the Arraymancer contributors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# This file is modified by William Whitacre, and these changes are
+# (C) WINDGO, Inc. 2018 under the MIT license. Since we have need for a static
+# array which behaves like a sequence, but are not using it for a numerical
+# purpose, it seemed appropriate to generalize further.
+
+
+import system, sequtils
+
+
+type FixedSeq*[T; N: static[Natural]] = object
+    ## Custom stack allocated array that behaves like seq.
+    data*: array[N, T]
+    len*: Natural
+
+
+proc copyFrom*[A: FixedSeq](a: var A, s: varargs[A.T]) {.inline.} =
+  a.len = s.len
+  for i in 0..<s.len:
+    a.data[i] = s[i]
+
+proc copyFrom*(a: var FixedSeq, s: FixedSeq) {.inline.} =
+  a.len = s.len
+  for i in 0..<s.len:
+    a.data[i] = s.data[i]
+
+proc setLen*[A: FixedSeq](a: var FixedSeq, len: Natural) {.inline.} =
+  when compileOption("boundChecks"):
+    assert len <= A.N
+  a.len = len
+
+proc low*(a: FixedSeq): int {.inline.} =
+  0
+
+proc high*(a: FixedSeq): int {.inline.} =
+  a.len-1
+
+
+when NimVersion >= "0.17.3":
+  # Need to deal with BackwardsIndex and multi-type slice introduced by:
+  # https://github.com/nim-lang/Nim/commit/d52a1061b35bbd2abfbd062b08023d986dbafb3c
+
+  type Index = SomeSignedInt or BackwardsIndex
+  template `^^`(s, i: untyped): untyped =
+    when i is BackwardsIndex:
+      s.len - int(i)
+    else: int(i)
+else:
+  type Index = SomeSignedInt
+  template `^^`(s, i: untyped): untyped =
+    i
+
+  proc `^`*(x: SomeSignedInt; a: FixedSeq): int {.inline.} =
+    a.len - x
+
+proc `[]`*[A: FixedSeq](a: A, idx: Index): A.T {.inline.} =
+  a.data[a ^^ idx]
+
+proc `[]`*[A: FixedSeq](a: var A, idx: Index): var A.T {.inline.} =
+  a.data[a ^^ idx]
+
+proc `[]=`*[A: FixedSeq](a: var A, idx: Index, v: A.T) {.inline.} =
+  a.data[a ^^ idx] = v
+
+proc `[]`*[A: FixedSeq](a: A, slice: Slice[int]): A {.inline.} =
+  let bgn_slice = a ^^ slice.a
+  let end_slice = a ^^ slice.b
+
+  if end_slice >= bgn_slice:
+    result.len = (end_slice - bgn_slice + 1)
+    for i in 0..<result.len:
+      result[i] = a[bgn_slice+i]
+
+iterator items*[A: FixedSeq](a: A): A.T {.inline.} =
+  for i in 0..<a.len:
+    yield a.data[i]
+
+iterator mitems*[A: FixedSeq](a: var A): var A.T {.inline.} =
+  for i in 0..<a.len:
+    yield a.data[i]
+
+iterator pairs*[A: FixedSeq](a: A): (int, A.T) {.inline.} =
+  for i in 0..<a.len:
+    yield (i,a.data[i])
+
+proc `@`*[A: FixedSeq](a: A): seq[A.T] {.inline.} =
+  result = newSeq[A.T](a.len)
+  for i in 0..<a.len:
+    result[i] = a.data[i]
+
+proc `$`*(a: FixedSeq): string {.inline.} =
+  result = "["
+  var firstElement = true
+  for value in items(a):
+    if not firstElement: result.add(", ")
+    result.add($value)
+    firstElement = false
+  result.add("]")
+
+# Removed product procedure.
+
+proc insert*[A: FixedSeq](a: var A, value: A.T, index: Natural = 0) {.inline.} =
+  for i in countdown(a.len, index+1):
+    a[i] = a[i-1]
+  a[index] = value
+  inc a.len
+
+proc delete*(a: var FixedSeq, index: Natural) {.inline.} =
+  dec(a.len)
+  for i in index..<a.len:
+    a[i] = a[i+1]
+  a[a.len] = 0
+
+proc add*[A: FixedSeq](a: var A, value: A.T) {.inline.} =
+  a[a.len] = value
+  inc a.len
+
+proc `&`*[A: FixedSeq](a: A, value: A.T): A {.inline.} =
+  result = a
+  result.add(value)
+
+proc `&`*(a, b: FixedSeq): FixedSeq {.inline.} =
+  result = a
+  result.len += b.len
+  for i in 0..<b.len:
+    result[a.len + i] = b[i]
+
+proc reversed*(a: FixedSeq): FixedSeq {.inline.} =
+  for i in 0..<a.len:
+    result[a.len-i-1] = a[i]
+  result.len = a.len
+
+proc reversed*(a: FixedSeq, result: var FixedSeq) {.inline.} =
+  for i in 0..<a.len:
+    result[a.len-i-1] = a[i]
+  for i in a.len..<result.len:
+    result[i] = 0
+  result.len = a.len
+
+proc `==`*[A: FixedSeq](a: A, s: openarray[A.T]): bool {.inline.} =
+  if a.len != s.len:
+    return false
+  for i in 0..<s.len:
+    if a[i] != s[i]:
+      return false
+  return true
+
+proc `==`*(a, s: FixedSeq): bool {.inline.} =
+  if a.len != s.len:
+    return false
+  for i in 0..<s.len:
+    if a[i] != s[i]:
+      return false
+  return true
+
+iterator zip*[A, B: FixedSeq](a: A, b: B): (A.T, B.T)=
+  let len = min(a.len, b.len)
+  for i in 0..<len:
+    yield (a[i], b[i])
+
+proc concat*[A: FixedSeq](dsas: varargs[A]): A =
+  var total_len = 0
+  for dsa in dsas:
+    inc(total_len, dsa.len)
+
+  assert total_len <= A.N
+
+  result.len = total_len
+  var i = 0
+  for dsa in dsas:
+    for val in dsa:
+      result[i] = val
+      inc(i)
+
