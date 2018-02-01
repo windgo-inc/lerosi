490493d75ba554c7c6783e6e8c8828296d74ca0e
William Whitacre <w.whitacre@windgo.com>
2018-02-01 10:36:47 -0600

    Added tests for interleaved to planar and planar to interleaved channel copies using copyChannelsTo in lerosi/colorspace. Collapsed the redundancy of the copy case templates a bit further to be more DRY. No longer comparing channels by layout ID, which permits the swizzle map (of type ChannelIndexArray) to be computed at compile-time.

--- lerosi/colorspace.nim
+++ lerosi/colorspace.nim
@@ -19,30 +19,30 @@ template ch_copy_impl(src, dest, imap, asgn, slcr, op: untyped): untyped =
       let x = slcr(src, src_i)
       asgn(dest, dest_i, op(x))
 
-template pl_copy_impl(src, dest, imap, slcr, op: untyped): untyped =
+template pl_copy(src, dest, imap, slcr, op: untyped): untyped =
   ch_copy_impl(src, dest, imap, pl_slice_asgn, slcr, op)
-template il_copy_impl(src, dest, imap, slcr, op: untyped): untyped =
+template il_copy(src, dest, imap, slcr, op: untyped): untyped =
   ch_copy_impl(src, dest, imap, il_slice_asgn, slcr, op)
 
 template pslc_noop(x: untyped): untyped = x
 
-template ch_copy(src, srcOrder, dest, destOrder, imap: untyped): untyped =
-  case destOrder:
-    of OrderPlanar:
-      case srcOrder:
-        of OrderPlanar:      pl_copy_impl(src, dest, imap, pl_slicer, pslc_noop)
-        of OrderInterleaved: pl_copy_impl(src, dest, imap, il_slicer, to_chw)
-    of OrderInterleaved:
-      case srcOrder:
-        of OrderPlanar:      il_copy_impl(src, dest, imap, pl_slicer, to_hwc)
-        of OrderInterleaved: il_copy_impl(src, dest, imap, il_slicer, pslc_noop)
+template src_copy_impl(order, copyfn, pl_op, il_op, src, dest, imap: untyped): untyped =
+  if order == OrderPlanar:
+    copyfn(src, dest, imap, pl_slicer, pl_op)
+  else:
+    copyfn(src, dest, imap, il_slicer, il_op)
 
+template ch_copy(src, srcOrder, dest, destOrder, imap: untyped): untyped =
+  if destOrder == OrderPlanar:
+    src_copy_impl(srcOrder, pl_copy, pslc_noop, to_chw, src, dest, imap)
+  else:
+    src_copy_impl(srcOrder, il_copy, to_hwc, pslc_noop, src, dest, imap)
 
 template copyChannelsTo*(srcImg, destImg: untyped): untyped =
   ch_copy(
     srcImg.data,  srcImg.order,
     destImg.data, destImg.order,
-    cmpChannels(srcImg.layoutId, destImg.layoutId)
+    cmpChannels(srcImg, destImg)
   )
 
 {.deprecated: [copyChannels: copyChannelsTo].}
